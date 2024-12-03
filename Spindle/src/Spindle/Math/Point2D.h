914091390b5f630e466dc13b16a7c6bf126b1d6d
Samantha Martorana <smartorana@logisolve.com>
2024-12-03 10:47:25 -0600

    Modifiy formatting in point 2d
    Changes whitespace in lerp, distance between, and distance squared between.

--- Spindle/src/Spindle/Math/Point2D.h
+++ Spindle/src/Spindle/Math/Point2D.h
@@ -11,11 +11,11 @@ namespace Spindle {
         // Constructors
         constexpr Point2D() noexcept
             : x{ 0.0f },
-            y{ 0.0f } {}
+              y{ 0.0f } {}
 
         Point2D(float p_x, float p_y)
             : x{ p_x },
-            y{ p_y } {}
+              y{ p_y } {}
 
         // Adding a vector results in a new point
         Point2D operator+(const Vector2<float>& vec) const {
@@ -29,9 +29,9 @@ namespace Spindle {
 
         // Linear interpolation
         Point2D lerp(const Point2D& other, float t) const {
-            __m128 p1 = SSE_Set(x, y, 0.0f, 0.0f);
-            __m128 p2 = SSE_Set(other.x, other.y, 0.0f, 0.0f);
-            __m128 tVec = _mm_set1_ps(t);
+            __m128     p1 = SSE_Set(x, y, 0.0f, 0.0f);
+            __m128     p2 = SSE_Set(other.x, other.y, 0.0f, 0.0f);
+            __m128   tVec = _mm_set1_ps(t);
             __m128 result = SSE_Add(p1, SSE_Multiply(SSE_Subtract(p2, p1), tVec));
 
             return Point2D(SSE_GetX(result), SSE_GetY(result));
@@ -53,15 +53,15 @@ namespace Spindle {
 
         // Distance between two points
         float distanceBetween(const Point2D& other) const {
-            __m128 p1 = SSE_Set(x, y, 0.0f, 0.0f);
-            __m128 p2 = SSE_Set(other.x, other.y, 0.0f, 0.0f);
+            __m128   p1 = SSE_Set(x, y, 0.0f, 0.0f);
+            __m128   p2 = SSE_Set(other.x, other.y, 0.0f, 0.0f);
             __m128 diff = SSE_Subtract(p1, p2);
             return std::sqrt(SSE_Dot(diff, diff));
         }
 
         float distanceSquaredBetween(const Point2D& other) const {
-            __m128 p1 = SSE_Set(x, y, 0.0f, 0.0f);
-            __m128 p2 = SSE_Set(other.x, other.y, 0.0f, 0.0f);
+            __m128   p1 = SSE_Set(x, y, 0.0f, 0.0f);
+            __m128   p2 = SSE_Set(other.x, other.y, 0.0f, 0.0f);
             __m128 diff = SSE_Subtract(p1, p2);
             return SSE_Dot(diff, diff);
         }
