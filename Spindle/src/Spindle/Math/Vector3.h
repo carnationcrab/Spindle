0eafb9816a90fcd190b7d2b1496d7a5bb7f57dd0
Samantha Martorana <smartorana@logisolve.com>
2024-12-07 18:37:52 -0600

    Add set vector 3 method
    same idea as set vector 2

--- Spindle/src/Spindle/Math/Vector3.h
+++ Spindle/src/Spindle/Math/Vector3.h
@@ -6,6 +6,12 @@
 #include <cmath>
 #include <string>
 
+/********************************
+*                               *
+*  Vector (3D)      (2,1,5)     *
+*                               *
+********************************/
+
 namespace Spindle {
 
     // all other datatypes use regular math
@@ -13,6 +19,13 @@ namespace Spindle {
     struct Vector3 {
     private:
         T x, y, z;
+
+
+    /**********************
+    *                     *
+    *    constructors     *
+    *                     *
+    **********************/
 
     public:
         constexpr Vector3() noexcept
@@ -24,6 +37,12 @@ namespace Spindle {
             : x{ p_x },
               y{ p_y },
               z{ p_z } {}
+
+    /**********************
+    *                     *
+    * operator overloads  *
+    *                     *
+    **********************/
 
         // addition
         constexpr Vector3 operator+(const Vector3& operand) const noexcept {
@@ -39,6 +58,12 @@ namespace Spindle {
         constexpr Vector3 operator*(T scalar) const noexcept {
             return Vector3(x * scalar, y * scalar, z * scalar);
         }
+
+    /**********************
+    *                     *
+    *       methods       *
+    *                     *
+    **********************/
 
         // dot product
         constexpr T dot(const Vector3& operand) const noexcept {
@@ -64,7 +89,7 @@ namespace Spindle {
             return std::sqrt(magnitudeSquared());
         }
 
-        // Collinear test (same direction)
+        // collinear test (same direction)
         bool isCollinear(const Vector3& operand, float epsilon = 1e-5) const noexcept {
             float dotProduct = dot(operand);
             float       magA = magnitude();
@@ -73,7 +98,7 @@ namespace Spindle {
             return std::abs(dotProduct - (magA * magB)) < epsilon;
         }
 
-        // Collinear but opposite
+        // collinear but opposite
         bool isCollinearOpposite(const Vector3& operand, float epsilon = 1e-5) const noexcept {
             float dotProduct = dot(operand);
             float       magA = magnitude();
@@ -82,22 +107,29 @@ namespace Spindle {
             return std::abs(dotProduct + (magA * magB)) < epsilon;
         }
 
-        // Perpendicular test
+        // perpendicular test
         bool isPerpendicular(const Vector3& operand, float epsilon = 1e-5) const noexcept {
             float dotProduct = dot(operand);
 
             return std::abs(dotProduct) < epsilon;
         }
 
-        // Same direction
+        // same direction
         bool isSameDirection(const Vector3& operand) const noexcept {
             return dot(operand) > 0;
         }
 
-        // Opposite direction
+        // opposite direction
         constexpr bool isOppositeDirection(const Vector3& operand) const noexcept {
             return dot(operand) < 0;
         }
+
+
+    /**********************
+    *                     *
+    *      utilities      *
+    *                     *
+    **********************/
 
         // string output
         std::string toString() const {
@@ -117,11 +149,24 @@ namespace Spindle {
         constexpr void setZ(T newZ) noexcept { z = newZ; }
     };
 
+/*************************************
+*                                    *
+*  Vector3<float> (3D)(2.0,1.0,5.0)  *
+*                                    *
+*************************************/
+
     // Vector3<float> uses SIMD
     template <>
     struct alignas(16) Vector3<float> {
     private:
         float x, y, z;
+
+
+    /**********************
+    *                     *
+    *    constructors     *
+    *                     *
+    **********************/
 
     public:
         Vector3() noexcept
@@ -133,6 +178,12 @@ namespace Spindle {
             : x{ p_x },
               y{ p_y },
               z{ p_z } {}
+
+    /**********************
+    *                     *
+    * operator overloads  *
+    *                     *
+    **********************/
 
         // addition (SIMD)
         Vector3 operator+(const Vector3<float>& operand) const noexcept {
@@ -152,29 +203,31 @@ namespace Spindle {
             return Vector3(
                 SSE_GetX(result),
                 SSE_GetY(result),
-                _mm_cvtss_f32(_mm_shuffle_ps(result, result, _MM_SHUFFLE(2, 2, 2, 2))));
+                SSE_GetZ(result));
         }
 
         // multiplication by a scalar (SIMD)
         Vector3 operator*(float scalar) const noexcept {
             __m128 a = SSE_Set(x, y, z, 0.0f);
-            __m128 result = SSE_MultiplyScalar(a, scalar);
+            __m128 result = SSE_Multiply(a, scalar);
             return Vector3(
                 SSE_GetX(result),
                 SSE_GetY(result),
-                _mm_cvtss_f32(_mm_shuffle_ps(result, result, _MM_SHUFFLE(2, 2, 2, 2))));
+                SSE_GetZ(result));
         }
+
+    /**********************
+    *                     *
+    *       methods       *
+    *                     *
+    **********************/
 
         // normalized vector, unit vector
         Vector3 unitVector() const noexcept {
             __m128      a = SSE_Set(x, y, z, 0.0f);
-            __m128 result = SSE_MultiplyScalar(a, (1 / magnitude()));
+            __m128 result = SSE_Multiply(a, (1 / magnitude()));
 
-            return Vector3(
-                SSE_GetX(result),
-                SSE_GetY(result),
-                SSE_GetZ(result)
-            );
+            return setVector3(result);
         }
 
         // cross product (not SIMD accelerated)
@@ -195,9 +248,7 @@ namespace Spindle {
         }
 
         // magnitude squared constexpr?
-        float magnitudeSquared() const noexcept {
-            return dot(*this);
-        }
+        float magnitudeSquared() const noexcept { return dot(*this); }
 
         // magnitude
         float magnitude() const noexcept {
@@ -207,16 +258,18 @@ namespace Spindle {
         // Collinear test (same direction)
         bool isCollinear(const Vector3& operand, float epsilon = 1e-5) const noexcept {
             float dotProduct = dot(operand);
-            float magA = magnitude();
-            float magB = operand.magnitude();
+            float       magA = magnitude();
+            float       magB = operand.magnitude();
+            
             return std::abs(dotProduct - (magA * magB)) < epsilon;
         }
 
         // Collinear but opposite
         bool isCollinearOpposite(const Vector3& operand, float epsilon = 1e-5) const noexcept {
             float dotProduct = dot(operand);
-            float magA = magnitude();
-            float magB = operand.magnitude();
+            float       magA = magnitude();
+            float       magB = operand.magnitude();
+            
             return std::abs(dotProduct + (magA * magB)) < epsilon;
         }
 
@@ -236,12 +289,27 @@ namespace Spindle {
             return dot(operand) < 0;
         }
 
+    /**********************
+    *                     *
+    *      utilities      *
+    *                     *
+    **********************/
+
         // "(x, y, z)"
         std::string toString() const noexcept {
             return "(" + 
                 std::to_string(x) + ", "
               + std::to_string(y) + ", " 
               + std::to_string(z) + ")";
+        }
+
+
+        Vector3 setVector3(__m128 result) const noexcept {
+            return Vector3(
+                SSE_GetX(result),
+                SSE_GetY(result),
+                SSE_GetZ(result)
+            );
         }
 
         // getters
