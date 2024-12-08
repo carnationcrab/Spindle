183a53ca7f2fa7902e20a239c3f5e6193ec618bc
Samantha Martorana <smartorana@logisolve.com>
2024-12-07 18:32:53 -0600

    Add linear interpolation to point2d

--- Spindle/src/Spindle/Math/Point2D.h
+++ Spindle/src/Spindle/Math/Point2D.h
@@ -4,40 +4,46 @@
 #include <cmath>
 #include <string>
 
+/**************************
+*                         *
+* Point (2D)   (2.0,1.0)  *
+*                         *
+**************************/
+
 namespace Spindle {
     struct Point2D {
         float x, y;
 
-        // Constructors
+    /**********************
+    *                     *
+    *    constructors     *
+    *                     *
+    **********************/
+
         constexpr Point2D() noexcept
             : x{ 0.0f },
-            y{ 0.0f } {}
+              y{ 0.0f } {}
 
         Point2D(float p_x, float p_y)
             : x{ p_x },
-            y{ p_y } {}
+              y{ p_y } {}
 
-        // Adding a vector results in a new point
+
+    /**********************
+    *                     *
+    * operator overloads  *
+    *                     *
+    **********************/
+
         Point2D operator+(const Vector2<float>& vec) const {
             return Point2D(x + vec.getX(), y + vec.getY());
         }
 
-        // Subtracting another point yields a vector
         Vector2<float> operator-(const Point2D& other) const {
             return Vector2<float>(x - other.x, y - other.y);
         }
 
-        // Linear interpolation
-        Point2D lerp(const Point2D& other, float t) const {
-            __m128     p1 = SSE_Set(x, y, 0.0f, 0.0f);
-            __m128     p2 = SSE_Set(other.x, other.y, 0.0f, 0.0f);
-            __m128   tVec = _mm_set1_ps(t);
-            __m128 result = SSE_Add(p1, SSE_Multiply(SSE_Subtract(p2, p1), tVec));
-
-            return Point2D(SSE_GetX(result), SSE_GetY(result));
-        }
-
-        // Equality checks
+        // equality checks
         bool operator==(const Point2D& other) const {
             return x == other.x && y == other.y;
         }
@@ -46,29 +52,54 @@ namespace Spindle {
             return !(*this == other);
         }
 
-        // Height above a line (simplified for now)
+    /**********************
+    *                     *
+    *       methods       *
+    *                     *
+    **********************/
+
+        // height (simple math todo upgrade)
         float height(const Point2D& other) const {
             return other.y - y;
         }
 
-        // Distance between two points
+        // distance between two points
         float distanceBetween(const Point2D& other) const {
             __m128   p1 = SSE_Set(x, y, 0.0f, 0.0f);
             __m128   p2 = SSE_Set(other.x, other.y, 0.0f, 0.0f);
+
             __m128 diff = SSE_Subtract(p1, p2);
             return std::sqrt(SSE_Dot(diff, diff));
         }
 
+        // squared
         float distanceSquaredBetween(const Point2D& other) const {
             __m128   p1 = SSE_Set(x, y, 0.0f, 0.0f);
             __m128   p2 = SSE_Set(other.x, other.y, 0.0f, 0.0f);
+
             __m128 diff = SSE_Subtract(p1, p2);
             return SSE_Dot(diff, diff);
         }
 
-        // String representation
+        // linear interpolate
+        Point2D lerp(const Point2D& other, float t) const {
+            __m128     p1 = SSE_Set(x, y, 0.0f, 0.0f);
+            __m128     p2 = SSE_Set(other.x, other.y, 0.0f, 0.0f);
+            __m128   tVec = _mm_set1_ps(t);
+
+            __m128 result = SSE_Add(p1, SSE_Multiply(SSE_Subtract(p2, p1), tVec));
+            return Point2D(SSE_GetX(result), SSE_GetY(result));
+        }
+
+    /**********************
+    *                     *
+    *      utilities      *
+    *                     *
+    **********************/
+
         std::string toString() const {
             return "(" + std::to_string(x) + ", " + std::to_string(y) + ")";
         }
+
     };
 }
